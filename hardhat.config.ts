77ddf0aba6fe188e68a4d30c2cdc35d9bca33880
kalpkran5 <113296618+kalpkran5@users.noreply.github.com>
2025-10-21 08:14:42 +0300

    Update hardhat.config.ts
    fix: updated hardhat setup for local testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,3 +25,4 @@ export default config;
 
 # feat: added base project structure for FHEVM testing
 # chore: upload initial configuration and env templates
+# fix: updated hardhat setup for local testing
